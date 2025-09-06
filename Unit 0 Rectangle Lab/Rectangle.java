0aa49c7d3d4a2ea831d8454bfbf74dd111d871b6
knantamanasikarn1-commits <knantamanasikarn1@hwemail.com>
2025-09-06 15:31:08 -0700

    Update Rectangle.java

--- Unit 0 Rectangle Lab/Rectangle.java
+++ Unit 0 Rectangle Lab/Rectangle.java
@@ -1,7 +1,11 @@
 public class Rectangle {
 
+    // Instance variables
+
     private int length;
     private int width;
+
+    // Constructors
 
     public Rectangle(int newLength, int newWidth) {
         this.length = newLength;
@@ -13,9 +17,49 @@ public class Rectangle {
         this.width = 4;
     }
 
+    // Getters
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    // Setters
+
+    public void setLength(int newLength) {
+        length = newLength;
+    }
+
+    public void setWidth(int newWidth) {
+        width = newWidth;
+    }
+
+    // Methods
+
     public String toString() {
-        int area = length * width;
-        return "This rectangle has a length of " + length + " and a width of " + width + ". It's area is " + area + ".";
+        return "This rectangle has a length of " + length + " and a width of " + width + ". It's area is " + calculateArea() + ".";
+    }
+
+    public boolean equals(Rectangle other) {
+        if (length == other.getLength() && width == other.getWidth()) {
+            return true;
+        }
+        return false;
+    }
+
+    public int calculateArea() {
+        return length * width;
+    }
+
+    public int calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    public double calculateDiagonal() {
+        return Math.sqrt(length ^ 2 + width ^ 2);
     }
 
 }
