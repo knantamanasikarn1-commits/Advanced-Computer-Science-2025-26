6e77b9f3fd8ac4ebdc17b023d592a5ce1338a8b2
knantamanasikarn1-commits <knantamanasikarn1@hwemail.com>
2025-09-08 13:26:57 -0700

    Math.pow() rect update

--- Unit 0 Rectangle Lab/Rectangle.java
+++ Unit 0 Rectangle Lab/Rectangle.java
@@ -40,7 +40,8 @@ public class Rectangle {
     // Methods
 
     public String toString() {
-        return "This rectangle has a length of " + length + " and a width of " + width + ". It's area is " + getArea() + ".";
+        return "This rectangle has a length of " + length + " and a width of " + 
+        width + ". It's area is " + getArea() + ".";
     }
 
     public boolean equals(Rectangle other) {
@@ -59,7 +60,7 @@ public class Rectangle {
     }
 
     public double getDiagonal() {
-        return Math.sqrt(length ^ 2 + width ^ 2);
+        return Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
     }
 
 }
