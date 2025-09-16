dd2143504370d46463e3ce3ade387714696057b5
knantamanasikarn1-commits <knantamanasikarn1@hwemail.com>
2025-09-16 13:26:45 -0700

    more id

--- Unit 0 Student Profiles Lab/Student.java
+++ Unit 0 Student Profiles Lab/Student.java
@@ -70,9 +70,7 @@ public class Student {
 
     public String generateId() {
         String returnedString = "";
-        for (int i = 0; i < 3; i++) {
-            returnedString += (int) (Math.random() * 9 + 1);
-        }
+        returnedString += (int) (Math.random() * 800 + 100);
         returnedString += "-";
         for (int i = 0; i < 4; i++) {
             returnedString += (int) (Math.random() * 10);
