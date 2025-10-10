b6d3b1bc5c9e3c6f407b1e6aa8153a93ac746e6c
knantamanasikarn1-commits <knantamanasikarn1@hwemail.com>
2025-10-10 13:50:04 -0700

    dangit I forgot a line

--- Unit 1/Dog Lab/Dog.java
+++ Unit 1/Dog Lab/Dog.java
@@ -57,6 +57,7 @@ public class Dog {
 
     public void setDogId(int dogId) {
         this.dogId = PawesomeUtils.validateDogId(dogId);
+        this.dogChar = PawesomeUtils.generateDogChar(this.dogId);
         this.dogTag = PawesomeUtils.generateDogTag(this.dogId, this.dogChar);
     }
 
