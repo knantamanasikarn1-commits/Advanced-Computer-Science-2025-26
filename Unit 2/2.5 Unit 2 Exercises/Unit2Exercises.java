96b0ccf1f9eca9e723767c1703db6811e58bf736
knantamanasikarn1-commits <knantamanasikarn1@hwemail.com>
2025-12-01 13:49:24 -0800

    fixes

--- Unit 2/2.5 Unit 2 Exercises/Unit2Exercises.java
+++ Unit 2/2.5 Unit 2 Exercises/Unit2Exercises.java
@@ -31,11 +31,17 @@ public class Unit2Exercises {
 
     public static String fizzString(String str) {
         String returnedString = "";
+        boolean returnOriginal = true;
         if (str.charAt(0) == 'f') {
             returnedString += "Fizz";
+            returnOriginal = false;
         }
         if (str.charAt(str.length() - 1) == 'b') {
             returnedString += "Buzz";
+            returnOriginal = false;
+        }
+        if (returnOriginal) {
+            return str;
         }
         return returnedString;
     }
@@ -65,7 +71,7 @@ public class Unit2Exercises {
 
     public static boolean catDog(String str) {
         if (str.length() <= 2) {
-            return false;
+            return true;
         }
         int dogCount = 0;
         int catCount = 0;
@@ -116,9 +122,9 @@ public class Unit2Exercises {
         a = a.toLowerCase();
         b = b.toLowerCase();
         if (a.length() > b.length()) {
-            return (b.substring(b.length() - a.length()).equals(a));
+            return (a.substring(a.length() - b.length()).equals(b));
         }
-        return (a.substring(a.length() - b.length()).equals(b));
+        return (b.substring(b.length() - a.length()).equals(a));
     }
 
     public static int countCode(String str) {
@@ -126,7 +132,7 @@ public class Unit2Exercises {
             return 0;
         }
         int codeCount = 0;
-        for (int i = 0; i < str.length() - 4; i++) {
+        for (int i = 0; i <= str.length() - 4; i++) {
             if (str.charAt(i) == 'c' && str.charAt(i + 1) == 'o' && str.charAt(i + 3) == 'e') {
                 codeCount += 1;
             }
@@ -147,9 +153,9 @@ public class Unit2Exercises {
     public static int bigDiff(int[] nums) {
         int currentMin = nums[0];
         int currentMax = nums[0];
-        for (int i = 0; i < nums.length - 1; i++) {
-            currentMin = Math.min(nums[i], nums[i + 1]);
-            currentMax = Math.max(nums[i], nums[i + 1]);
+        for (int i = 0; i < nums.length; i++) {
+            currentMin = Math.min(nums[i], currentMin);
+            currentMax = Math.max(nums[i], currentMax);
         }
         return currentMax - currentMin;
     }
@@ -224,32 +230,79 @@ public class Unit2Exercises {
     }
 
     public static String[] wordsWithout(String[] words, String target) {
-        // to-do: implement this method
-        return new String[0];
+        int targetCount = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (target.equals(words[i])) {
+                targetCount += 1;
+            }
+        }
+        String[] newArray = new String[words.length - targetCount];
+        int currentIndexPut = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (! target.equals(words[i])) {
+                newArray[currentIndexPut] = words[i];
+                currentIndexPut += 1;
+            }
+        }
+        return newArray;
+    }
+
+    public static int average(int[] scores, int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            sum += scores[i];
+        }
+        return sum / (end - start + 1);
     }
 
     public static int scoresAverage(int[] scores) {
-        // to-do: implement this method
-        return 0;
+        int firstAverage = average(scores, 0, scores.length / 2 - 1);
+        int secondAverage = average(scores, scores.length / 2, scores.length - 1);
+        return Math.max(firstAverage, secondAverage);
     }
 
     public static boolean scoresIncreasing(int[] scores) {
-        // to-do: implement this method
-        return false;
+        for (int i = 0; i < scores.length - 1; i++) {
+            if (scores[i + 1] < scores[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int getLargestSpecial(int[] scores) {
+        int maxSpecial = 0;
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] % 10 != 0) {
+                continue;
+            }
+            maxSpecial = Math.max(maxSpecial, scores[i]);
+        }
+        return maxSpecial;
     }
 
     public static int scoresSpecial(int[] a, int[] b) {
-        // to-do: implement this method
-        return 0;
+        int aSpecial = getLargestSpecial(a);
+        int bSpecial = getLargestSpecial(b);
+        return aSpecial + bSpecial;
     }
 
     public static String firstTwo(String str) {
-        // to-do: implement this method
-        return "";
+        if (str.length() == 0) {
+            return "**";
+        }
+        if (str.length() == 1) {
+            return str + "*";
+        }
+        return str.substring(0, 2);
     }
 
     public static double divide(int a, int b) {
-        // to-do: implement this method
-        return 0.0;
+        int larger = Math.max(a, b);
+        int smaller = Math.min(a, b);
+        if (smaller == 0) {
+            return 0.0;
+        }
+        return (double) larger / smaller;
     }
 }
--- Unit 2/2.5 Unit 2 Exercises/Unit2ExercisesTester.java
+++ Unit 2/2.5 Unit 2 Exercises/Unit2ExercisesTester.java
@@ -1,9 +1,25 @@
 public class Unit2ExercisesTester {
     public static void main(String[] args) {
-        int[] test = {1, 2, 3, 0, 0, 4};
-        int[] newArray = Unit2Exercises.zeroFront(test);
-        for (int i = 0; i < test.length; i++) {
-            System.out.println(newArray[i]);
-        }
+        System.out.println(Unit2Exercises.alarmClock(1, false)); // 7:00
+        System.out.println(Unit2Exercises.alarmClock(5, false)); // 7:00
+        System.out.println(Unit2Exercises.alarmClock(0, false)); // 10:00
+        System.out.println();
+        System.out.println(Unit2Exercises.love6(6, 4)); // true
+        System.out.println(Unit2Exercises.love6(4, 5)); // false
+        System.out.println(Unit2Exercises.love6(1, 5)); // true
+        System.out.println();
+        System.out.println(Unit2Exercises.redTicket(2, 2, 2)); // 10
+        System.out.println(Unit2Exercises.redTicket(2, 2, 1)); // 0
+        System.out.println(Unit2Exercises.redTicket(0, 0, 0)); // 5
+        System.out.println();
+        System.out.println(Unit2Exercises.fizzString("fig")); // Fizz
+        System.out.println(Unit2Exercises.fizzString("dib")); // Buzz
+        System.out.println(Unit2Exercises.fizzString("fib")); // FizzBuzz
+        System.out.println();
+        System.out.println(Unit2Exercises.doubleChar("The"));
+        System.out.println(Unit2Exercises.doubleChar("AAbb"));
+        System.out.println(Unit2Exercises.doubleChar("Hi-There"));
+        System.out.println();
+        System.out.println(Unit2Exercises.countHi(null));
     }
 }
